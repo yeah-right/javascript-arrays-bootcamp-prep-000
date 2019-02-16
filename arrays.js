5177620b8dac6a5b078a58a56a06c0e6c6e369a0
Zack A Claar <claar.zack@gmail.com>
2019-02-16 15:44:40 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,42 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
+
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array];
+}
+
+// function destructivelyAddElementToBeginningOfArray(array, element) {
+//   return array.unshift(element);
+// }
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  
+}
+
+function addElementToEndOfArray(array, element) {
+  return [...array, element]; 
+}
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  return array.push(element);
+}
+
+function accessElementInArray(array, i) {
+  return array[i];
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  return array.shift();
+}
+
+function removeElementFromBeginningOfArray(array) {
+  return array.slice(1);
+}
+
+function destructivelyRemoveElementFromEndOfArray(array) {
+  return array.pop();
+}
+
+function removeElementFromEndOfArray(array) {
+  return array.slice(0, array.length -1);
+}
+
